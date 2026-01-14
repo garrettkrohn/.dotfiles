4d6cfa1e09267f3f979afb5c6bbd1a74885cc001
Garrett Krohn <garrettkrohn@gmail.com>
2026-01-14 13:09:32 -0600

    add diff view

--- config/kickstart/lua/plugins/diffview.lua
+++ config/kickstart/lua/plugins/diffview.lua
@@ -2,4 +2,26 @@ return {
   lazy = true,
   'sindrets/diffview.nvim',
   cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewToggleFiles', 'DiffviewFocusFiles' },
+  config = function()
+    local actions = require 'diffview.actions'
+    require('diffview').setup {
+      file_panel = {
+        listing_style = 'list', -- or "list"
+        position = 'bottom', -- "left", "right", "top", "bottom"
+        width = 35, -- width for left/right
+        height = 10, -- height for top/bottom
+      },
+      keymaps = {
+        view = {
+          -- Navigate to next/previous file
+          { 'n', ']q', actions.select_next_entry, { desc = 'Next file' } },
+          { 'n', '[q', actions.select_prev_entry, { desc = 'Previous file' } },
+        },
+        file_panel = {
+          { 'n', ']q', actions.select_next_entry, { desc = 'Next file' } },
+          { 'n', '[q', actions.select_prev_entry, { desc = 'Previous file' } },
+        },
+      },
+    }
+  end,
 }
