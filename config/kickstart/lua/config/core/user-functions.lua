78756aec94a5ce2da0cd9677cfeadf982e03308c
Garrett Krohn <garrettkrohn@gmail.com>
2026-02-04 20:29:27 -0600

    update plugins

--- config/kickstart/lua/config/core/user-functions.lua
+++
@@ -1,4 +0,0 @@
--- Load all user functions from user/functions directory
-for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath 'config' .. '/lua/user/functions', [[v:val =~ '\.lua$']])) do
-  require('user.functions.' .. file:gsub('.lua$', ''))
-end
--- config/kickstart/lua/plugins/editing/scissors.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'chrisgrieser/nvim-scissors',
-  enabled = false,
-  dependencies = 'nvim-telescope/telescope.nvim', -- optional
-  opts = { snippetDir = vim.fn.stdpath 'data' .. '/lazy/friendly-snippets' },
-}
