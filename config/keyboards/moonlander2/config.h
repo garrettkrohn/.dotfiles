62539976358813645cdfc8d5dae7163db3f9d329
Garrett Krohn <garrettkrohn@gmail.com>
2026-02-08 21:16:17 -0600

    qmk: v2.0.0

---
+++ config/keyboards/moonlander2/config.h
@@ -0,0 +1,12 @@
+#undef TAPPING_TERM
+#define TAPPING_TERM 140
+
+#define COMBO_TERM 20
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define SERIAL_NUMBER "wavoD/B4Ewnv"
+#define LAYER_STATE_8BIT
+#define COMBO_COUNT 7
+
+#define RGB_MATRIX_STARTUP_SPD 60
+
